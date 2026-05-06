41a1f10689ea39d2e1d5fd0f708950841dd8e173
neinkopp <jakob.schleitzer@gmail.com>
2026-05-06 12:58:22 +0200

    Refactor ProviderFactory::make to specify return type and improve readability

--- app/Services/Providers/ProviderFactory.php
+++ app/Services/Providers/ProviderFactory.php
@@ -8,9 +8,9 @@ use InvalidArgumentException;
 
 class ProviderFactory
 {
-	public static function make(string $supplierName)
+	public static function make(string $supplierName): PrintProviderStrategy
 	{
-		match ($supplierName) {
+		return match ($supplierName) {
 			"default_api_nodata" => new DefaultAPINoDataStrategy(),
 			"default_api_data" => new DefaultAPIDataStrategy(),
 			default => throw new InvalidArgumentException("Dienstleister $supplierName nicht gefunden")
