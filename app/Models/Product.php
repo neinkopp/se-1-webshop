f54768ffd69483fa6b2c0b773862e0fde4ad8e5e
neinkopp <jakob.schleitzer@gmail.com>
2026-05-07 10:23:55 +0200

    Refactor Product model methods for consistency and readability

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,5 +1,7 @@
 <?php
-declare (strict_types = 1);
+
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -7,23 +9,28 @@ use Illuminate\Support\Str;
 
 class Product extends Model
 {
-    public function getProductCards(?int $category_id = null, array ...$filteredAttributes): array {
+    public function getProductCards(?int $category_id = null, array ...$filteredAttributes): array
+    {
         return [];
     }
 
-    public function getFeaturedProductCards(): array {
+    public function getFeaturedProductCards(): array
+    {
         return [];
     }
 
-    private function deserializeAttributes(array $product): array {
+    private function deserializeAttributes(array $product): array
+    {
         return [];
     }
 
-    public function getProductByHandle(string $handle): array {
+    public function getProductByHandle(string $handle): array
+    {
         return [];
     }
 
-    public function getProductById(string $uuid): array {
+    public function getProductById(string $uuid): array
+    {
         return [];
     }
 }
