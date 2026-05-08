8c04aa6fba68b24cdddc3139ca2d1680fd275acc
neinkopp <jakob.schleitzer@gmail.com>
2026-05-08 17:47:42 +0200

    Add component-test view and route for testing UI components add button preview WIP

---
+++ resources/views/component-test.blade.php
@@ -0,0 +1,23 @@
+<script src="https://cdn.tailwindcss.com"></script>
+
+<div class="p-24 bg-gray-50 h-full">
+	<div class="flex flex-col gap-4">
+		<div>
+		</div>
+		<div>
+			<button class="text-white px-4 py-2 rounded-md transition ease-in-out bg-[#003063] hover:bg-[#106484]">Hinzufügen</button>
+		</div>
+		<div>
+			<button class="text-[#003063] px-4 py-2 rounded-md transition ease-in-out bg-white hover:bg-[#b8e0e3]">Hinzufügen</button>
+		</div>
+		<div>
+			<button class="text-[#003063] px-4 py-2 rounded-md transition ease-in-out bg-white hover:bg-[#b8e0e3] border-2 border-solid border-[#003063]">Hinzufügen</button>
+		</div>
+		<div>
+			<button class="text-white px-8 py-3 w-full rounded-md transition ease-in-out bg-[#003063] hover:bg-[#106484]">Zur Kasse</button>
+		</div>
+		<div>
+			<button class="text-[#003063] px-8 py-3 rounded-md transition ease-in-out bg-white hover:bg-[#b8e0e3] border-2 border-solid border-[#003063]">Hinzufügen</button>
+		</div>
+	</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Models\Product;
 
 Route::get('/', [ProductListController::class, "show"]);
 
+Route::get("/component-test", function () {
+    return view('component-test');
+});
+
 Route::get('/products', [ProductListController::class, "show"]);
 
 Route::get('/products/{productHandle}', function ($productHandle) {
@@ -14,35 +18,35 @@ Route::get('/products/{productHandle}', function ($productHandle) {
         [
             'productHandle' => 'mockup_product_1',
             'productDisplayName' => 'Mertens\'s Sack',
-            'productDisplayPrice' => rand(0,9).rand(0,9).','.rand(0,9).rand(0,9),
+            'productDisplayPrice' => rand(0, 9) . rand(0, 9) . ',' . rand(0, 9) . rand(0, 9),
             'productDescription' => 'Hier passt viel rein und bequem fühlt er sich auch an. Frauen lieben ihn!',
             'productImagePath' => 'resources/images/mockup_product_1.png',
         ],
         [
             'productHandle' => 'mockup_product_2',
             'productDisplayName' => 'Mertens\'s T-Shirt mit extra Ehre',
-            'productDisplayPrice' => rand(0,9).rand(0,9).','.rand(0,9).rand(0,9),
+            'productDisplayPrice' => rand(0, 9) . rand(0, 9) . ',' . rand(0, 9) . rand(0, 9),
             'productDescription' => 'Du brauchst Aura-Punkte auf schnelle-welle? Dieses T-Shirt ist absolut ehrenhaft und gibt dir den Look, den ein wahrer Löwe braucht. Rawr!',
             'productImagePath' => 'resources/images/mockup_product_2.png',
         ],
         [
             'productHandle' => 'mockup_product_3',
             'productDisplayName' => 'Magischer Teppich des Mertens',
-            'productDisplayPrice' => rand(0,9).rand(0,9).','.rand(0,9).rand(0,9),
+            'productDisplayPrice' => rand(0, 9) . rand(0, 9) . ',' . rand(0, 9) . rand(0, 9),
             'productDescription' => 'Kann er fliegen? Wer weiß das schon? Er sieht schick aus und ist leicht zu reinigen!',
             'productImagePath' => 'resources/images/mockup_product_3.png',
         ],
         [
             'productHandle' => 'mockup_product_4',
             'productDisplayName' => 'Bettwäsche und Kissen mit dem Gesicht von Mertens',
-            'productDisplayPrice' => rand(0,9).rand(0,9).','.rand(0,9).rand(0,9),
+            'productDisplayPrice' => rand(0, 9) . rand(0, 9) . ',' . rand(0, 9) . rand(0, 9),
             'productDescription' => 'Stell dir vor, du wachst auf und wirst begrüßt vom göttlichen Anblick Mertens. Zu einem guten Schlaf gehört immer auch ein Professor!',
             'productImagePath' => 'resources/images/mockup_product_4.png',
         ],
         [
             'productHandle' => 'mockup_product_5',
             'productDisplayName' => 'X-Streme Mertens Energy Drink',
-            'productDisplayPrice' => rand(0,9).rand(0,9).','.rand(0,9).rand(0,9),
+            'productDisplayPrice' => rand(0, 9) . rand(0, 9) . ',' . rand(0, 9) . rand(0, 9),
             'productDescription' => 'Für den X-Stremen Kick in die Nacht. Dieser Schmackofatz liefert dir die Energie, die du während deiner Fortnite-Runde brauchst. Auf den Sieg! Auf Mertens\'s X-Streme!',
             'productImagePath' => 'resources/images/mockup_product_5.png',
         ]
@@ -55,5 +59,4 @@ Route::get('/products/{productHandle}', function ($productHandle) {
     }
 
     return view('product', compact('product'));
-
 });
