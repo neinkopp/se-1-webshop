41a25edcb5a4bdf3e2e2cf105a83953333cdc8b9
neinkopp <jakob.schleitzer@gmail.com>
2026-05-08 14:11:01 +0200

    Fix ProductTest with additional product attributes

--- tests/Feature/View/ProductTest.php
+++ tests/Feature/View/ProductTest.php
@@ -2,17 +2,28 @@
 
 namespace Tests\Feature\View;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic view test example.
      */
     public function test_it_can_render(): void
     {
+        $this->withoutVite();
+
+        $dummyProduct = [
+            'productImagePath' => 'resources/images/test-product.png',
+            'productDisplayName' => 'Test Produkt',
+            'productDescription' => "Dies ist eine description",
+            'productDisplayPrice' => '19.99'
+        ];
+
         $contents = $this->view('product', [
-            //
+            'product' => $dummyProduct // Hier die Variable übergeben!
         ]);
 
         $contents->assertSee('');
