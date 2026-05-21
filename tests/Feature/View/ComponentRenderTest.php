8a1127ecd96bbe82b04e77f7a4058bedbdb20eb7
jgruber <j.gruber@stud.bhh.de>
2026-05-21 08:48:07 +0200

    Added component unit tests

--- tests/Feature/View/ComponentRenderTest.php
+++ tests/Feature/View/ComponentRenderTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\View;
 
+use App\Models\Product;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -12,8 +14,349 @@ class ComponentRenderTest extends TestCase
     public function test_button_render(): void
     {
         $this->withoutVite();
-        $contents = $this->blade('<x-button>Test</x-button>');
+        $contents = $this->view('components.button', ['slot' => 'Test']);
 
         $contents->assertSee('');
     }
+
+    public function banner_carousel_render(): void
+    {
+        $this->withoutVite();
+        $contents = $this->view('components.banner-carousel', []);
+
+        $contents->assertSee('');
+    }
+
+    public function checkout_supplier_card_render(): void
+    {
+        $this->withoutVite();
+        $contents = $this->view('components.checkout-supplier-card', []);
+
+        $contents->assertSee('');
+    }
+
+    public function content_header_render(): void
+    {
+        $this->withoutVite();
+        $contents = $this->view('components.content-header', ['sidePanelTitle' => 'Title', 'categories' => []]);
+
+        $contents->assertSee('');
+    }
+
+    public function dropdown_list_render(): void
+    {
+        $this->withoutVite();
+        $contents = $this->view('components.dropdown-list', ['slot' => '', 'name' => 'name']);
+
+        $contents->assertSee('');
+    }
+
+    public function dropdown_option_render(): void
+    {
+        $this->withoutVite();
+        $contents = $this->view('components.dropdown-option', ['value' => 'value', 'displayText' => 'displayText']);
+
+        $contents->assertSee('');
+    }
+
+    public function featured_product_card_render(): void
+    {
+        $this->withoutVite();
+        $product = Product::factory();
+
+        $contents = $this->view('components.featured-product-card', ['product' => $product]);
+
+        $contents->assertSee('');
+    }
+
+    public function featured_products_render(): void
+    {
+        $this->withoutVite();
+        $product = Product::factory();
+
+        $contents = $this->view('components.featured-product-card', ['featuredProducts' => [$product]]);
+
+        $contents->assertSee('');
+    }
+
+    use InteractsWithViews;
+
+    public function test_filter_container_render(): void
+    {
+        $this->withoutVite();
+        
+        $filters = [
+            'brand' => [
+                'displayName' => 'Marke',
+                'type' => 'select',
+                'options' => ['Nike', 'Adidas']
+            ]
+        ];
+        
+        $contents = $this->view('components.filter-container', [
+            'filters' => $filters,
+            'category' => 'Electronics'
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_filter_range_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.filter-range', [
+            'min' => '10',
+            'max' => '100',
+            'minValue' => '0',
+            'maxValue' => '500'
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_filter_section_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.filter-section', [
+            'title' => 'Test Category',
+            'slot' => 'Test'
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_filter_select_render(): void
+    {
+        $this->withoutVite();
+        
+        $options = ['Option 1', 'Option 2'];
+        
+        $contents = $this->view('components.filter-select', [
+            'title' => 'test_filter',
+            'options' => $options,
+            'selected' => ['Option 1']
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_footer_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.footer');
+
+        $contents->assertSee('');
+    }
+
+    public function test_header_actions_mobile_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.header-actions-mobile');
+
+        $contents->assertSee('');
+    }
+
+    public function test_header_actions_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.header-actions');
+
+        $contents->assertSee('');
+    }
+
+    public function test_header_management_actions_mobile_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.header-management-actions-mobile');
+
+        $contents->assertSee('');
+    }
+
+    public function test_header_management_actions_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.header-management-actions');
+
+        $contents->assertSee('');
+    }
+
+    public function test_header_render(): void
+    {
+        $this->withoutVite();
+        
+        $actionsSlot = '<a href="/test">Test</a>';
+        $behaviorSlot = '<div>Behavior</div>';
+        
+        $contents = $this->view('components.header', [
+            'actionsSlot' => $actionsSlot,
+            'behaviorSlot' => $behaviorSlot
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_info_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.info', [
+            'slot' => 'Information message'
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_link_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.link', [
+            'slot' => 'Click Here',
+            'href' => '/test'
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_management_action_link_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.management-action-link', [
+            'href' => '/manage/dashboard',
+            'label' => 'Dashboard'
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_management_actions_container_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.management-actions-container');
+
+        $contents->assertSee('');
+    }
+
+    public function test_management_product_image_upload_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.management-product-image-upload', [
+            'href' => '/manage/product/1',
+            'label' => 'Product Image',
+            'image_source' => '/storage/images/product.jpg',
+            'deletion_href' => '/manage/product/1/delete',
+            'image_background_color' => 'blue-500',
+            'id' => '',
+            'name' => ''
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_management_product_input_number_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.management-product-input-number', [
+            'label' => 'Quantity',
+            'name' => 'quantity',
+            'value' => '42',
+            'required' => true,
+            'step' => '1'
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_management_product_input_option_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.management-product-input-option', [
+            'value' => 'option_value',
+            'label' => 'Option Label',
+            'selected' => true
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_management_product_input_select_render(): void
+    {
+        $this->withoutVite();
+        
+        $slotContent = '<option value="1">Option 1</option>';
+        
+        $contents = $this->view('components.management-product-input-select', [
+            'label' => 'Category',
+            'name' => 'category_id',
+            'value' => '1',
+            'required' => true,
+            'slot' => $slotContent
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_management_product_input_text_area_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.management-product-input-text-area', [
+            'label' => 'Description',
+            'name' => 'description',
+            'value' => 'Product description',
+            'required' => true
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_management_product_input_text_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.management-product-input-text', [
+            'label' => 'Product Name',
+            'name' => 'product_name',
+            'value' => 'Test Product',
+            'required' => true
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_sidebar_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.sidebar', [
+            'title' => 'Admin Panel',
+            'slot' => '<nav>Navigation</nav>'
+        ]);
+
+        $contents->assertSee('');
+    }
+
+    public function test_warning_render(): void
+    {
+        $this->withoutVite();
+        
+        $contents = $this->view('components.warning', [
+            'slot' => 'Warning message'
+        ]);
+
+        $contents->assertSee('');
+    }
+
 }
